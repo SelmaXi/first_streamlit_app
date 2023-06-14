0bf7b13f19f18c484166147b7bcd66874e6756e3
SelmaXi <134718274+SelmaXi@users.noreply.github.com>
2023-06-14 16:10:48 +0200

    Update streamlit_app.py
    new menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
 
-streamlit.title('Healthy food')
-streamlit.header('Breakfast menu')
-streamlit.text('Bluberry')
-streamlit.text('Spinach')
-streamlit.text('Egg')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
